110977e87505e205f49230b436c7bf5fe378bbcb
hucong <hucong@jd.com>
2017-12-04 02:10:32 +0800

    Modify by my mind

--- gulpfile.js
+++ gulpfile.js
@@ -90,7 +90,7 @@
         }))
         .pipe(gulp.dest('./public'));
     });
-    
+
     // 添加版本号
     gulp.task('rev', function() {
         return gulp.src('./public/**/*.html')
@@ -100,7 +100,7 @@
 
     // 同步执行task
     gulp.task('deploy',sequence(['minify-css','minify-js'],'rev','minify-html'));
-    
+
     // 部署前代码处理
     gulp.task('default',['deploy'],function(e){
        console.log("[complete] please execute： hexo d");
